00d9eae0326de6c6f033e5a80045c65d784a1bd4
wisdomblanket <yuhanraut@gmail.com>
2026-09-10 07:42:13 +0545

    Serve store at root URL

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'everest.html')));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
